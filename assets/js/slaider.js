235f9b6885cc25ae2057f6e000ae8296abac139b
jkiddd <ivan_x_@mail.ru>
2019-05-23 09:32:11 +0600

    слайдер написание

---
+++ assets/js/slaider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+   $('.slaider').slick(
+   {   autoplay:true,
+       autoplaySpeed:2000,
+       fade:true,
+       dots: true, 
+        
+   
+       }
+   );
+   });
+
