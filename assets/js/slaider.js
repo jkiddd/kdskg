f1f78e1aa4873c76a1177e97f66a1d406b694fcb
jkiddd <ivan_x_@mail.ru>
2019-05-23 13:49:27 +0600

    нижний слайд

--- assets/js/slaider.js
+++ assets/js/slaider.js
@@ -1,12 +1,18 @@
-$(document).ready(function(){
-   $('.slaider').slick(
-   {   autoplay:true,
-       autoplaySpeed:2000,
-       fade:true,
-       dots: true, 
-        
-   
-       }
-   );
-   });
+$(document).ready(function () {
+    $('.slaider').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        dots: true,
+    });
+});
 
+$(document).ready(function () {
+    $('.slaider-bottom').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        dots: false,
+        arrows:false,
+    });
+});
