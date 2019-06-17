879e2f25cf272d6a1c3ca833b4e36e2c4c560f7a
jkiddd <ivan_x_@mail.ru>
2019-06-17 16:52:46 +0600

    создание слайдера

---
+++ assets/js/slaider-carusel.js
@@ -0,0 +1,8 @@
+// $(document).ready(function () {
+//    $('.slaider-big').slick({
+//        autoplay: true,
+//        autoplaySpeed: 2000,
+//        fade: true,
+//        dots: true,
+//    });
+// });
