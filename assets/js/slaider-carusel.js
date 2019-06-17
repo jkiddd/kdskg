e45d48b6cc41aca427fd396f5b6333a866075269
jkiddd <ivan_x_@mail.ru>
2019-06-17 17:27:03 +0600

    реализация слайдера

--- assets/js/slaider-carusel.js
+++ assets/js/slaider-carusel.js
@@ -1,8 +1,20 @@
-// $(document).ready(function () {
-//    $('.slaider-big').slick({
-//        autoplay: true,
-//        autoplaySpeed: 2000,
-//        fade: true,
-//        dots: true,
-//    });
-// });
+$(document).ready(function () {
+   $('.slaider-big__wrap').slick({
+       autoplay: false,
+       autoplaySpeed: 2000,
+       fade: true,
+       dots: false,
+       arrows: true,
+       asNavFor: '.slaider-small__wrap'
+   });
+});
+$(document).ready(function () { 
+   $('.slaider-small__wrap').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      asNavFor: '.slaider-big__wrap',
+      dots: false,
+      centerMode: false,
+      focusOnSelect: true
+    });
+});
